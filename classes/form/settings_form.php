e88b9509796690057abad14b8536f417def8f573
Kenneth Hendricks <kennethhendricks@catalyst-au.net>
2016-12-09 16:03:56 +1100

    #5 fig region setting

--- classes/form/settings_form.php
+++ classes/form/settings_form.php
@@ -44,16 +44,16 @@ class settings_form extends \moodleform {
 
         $config = $this->_customdata['config'];
 
-        $regionoptions = array ('us-east-1',
-                                'us-east-2',
-                                'us-west-1',
-                                'us-west-2',
-                                'ap-northeast-2',
-                                'ap-southeast-1',
-                                'ap-southeast-2',
-                                'ap-northeast-1',
-                                'eu-central-1',
-                                'eu-west-1');
+        $regionoptions = array ('us-east-1' => 'us-east-1',
+                                'us-east-2' => 'us-east-2',
+                                'us-west-1' => 'us-west-1',
+                                'us-west-2' => 'us-west-2',
+                                'ap-northeast-2' => 'ap-northeast-2',
+                                'ap-southeast-1' => 'ap-southeast-1',
+                                'ap-southeast-2' => 'ap-southeast-2',
+                                'ap-northeast-1' => 'ap-northeast-1',
+                                'eu-central-1' => 'eu-central-1',
+                                'eu-west-1' => 'eu-west-1');
 
         $mform->addElement('advcheckbox', 'enabled', get_string('settings:enabled', 'tool_sssfs'));
         $mform->addHelpButton('enabled', 'settings:enabled', 'tool_sssfs');
