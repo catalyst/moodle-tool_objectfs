9ad228c55bf8eee014532135112a2a0bae50f162
Kenneth Hendricks <kennethhendricks@catalyst-au.net>
2017-02-24 11:46:24 +1100

    Fix travis issue

--- renderer.php
+++ renderer.php
@@ -36,7 +36,6 @@ class tool_objectfs_renderer extends plugin_renderer_base {
         $output .= \html_writer::link(new \moodle_url('/admin/tool/objectfs/index.php'), get_string('settings', 'tool_objectfs'));
         $output .= \html_writer::start_tag('br');
 
-
         $config = get_objectfs_config();
 
         if (!isset($config->enabletasks) || !$config->enabletasks) {
