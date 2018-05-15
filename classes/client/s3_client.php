7a09fe95cf2810990474467ea686110bbdded296
Alex Lawn <alex@catalyst-au.net>
2018-05-15 16:26:54 +1000

    Adding new AWS Regions

--- classes/client/s3_client.php
+++ classes/client/s3_client.php
@@ -296,16 +296,26 @@ class s3_client implements object_client {
 
         $mform = $this->define_amazon_s3_check($mform, $config);
 
-        $regionoptions = array( 'us-east-1'          => 'us-east-1',
-            'us-east-2'         => 'us-east-2',
-            'us-west-1'         => 'us-west-1',
-            'us-west-2'         => 'us-west-2',
-            'ap-northeast-2'    => 'ap-northeast-2',
-            'ap-southeast-1'    => 'ap-southeast-1',
-            'ap-southeast-2'    => 'ap-southeast-2',
-            'ap-northeast-1'    => 'ap-northeast-1',
-            'eu-central-1'      => 'eu-central-1',
-            'eu-west-1'         => 'eu-west-1');
+        $regionoptions = array(
+            'us-east-1'      => 'us-east-1 (N. Virginia)',
+            'us-east-2'      => 'us-east-2 (Ohio)',
+            'us-west-1'      => 'us-west-1 (N. California)',
+            'us-west-2'      => 'us-west-2 (Oregon)',
+            'ap-northeast-1' => 'ap-northeast-1 (Tokyo)',
+            'ap-northeast-2' => 'ap-northeast-2 (Seoul)',
+            'ap-northeast-3' => 'ap-northeast-3 (Osaka)',
+            'ap-south-1'     => 'ap-south-1 (Mumbai)',
+            'ap-southeast-1' => 'ap-southeast-1 (Singapore)',
+            'ap-southeast-2' => 'ap-southeast-2 (Sydney)',
+            'ca-central-1'   => 'ca-central-1 (Canda Central)',
+            'cn-north-1'     => 'cn-north-1 (Beijing)',
+            'cn-northwest-1' => 'cn-northwest-1 (Ningxia)',
+            'eu-central-1'   => 'eu-central-1 (Frankfurt)',
+            'eu-west-1'      => 'eu-west-1 (Ireland)',
+            'eu-west-2'      => 'eu-west-2 (London)',
+            'eu-west-3'      => 'eu-west-3 (Paris)',
+            'sa-east-1'      => 'sa-east-1 (Sao Paulo)'
+        );
 
         $mform->addElement('text', 's3_key', get_string('settings:aws:key', 'tool_objectfs'));
         $mform->addHelpButton('s3_key', 'settings:aws:key', 'tool_objectfs');
