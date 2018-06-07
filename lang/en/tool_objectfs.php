a85cbd8e307305636d85f4f2539f9da8724d2482
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2018-06-07 11:03:46 +1000

    Add new report for missing files to close issue #97

--- lang/en/tool_objectfs.php
+++ lang/en/tool_objectfs.php
@@ -38,7 +38,11 @@ $string['object_status:location'] = 'Object location';
 $string['object_status:files'] = 'Objects';
 $string['object_status:size'] = 'Total size';
 
-$string['object_status:location:error'] = 'Missing from filedir and external storage';
+$string['page:missingfiles'] = 'Missing from filedir and external storage files';
+
+$string['filename:missingfiles'] = 'missingfiles';
+
+$string['object_status:location:error'] = 'Missing from filedir and external storage (<a href="/admin/tool/objectfs/missing_files.php">view files</a>)';
 $string['object_status:location:duplicated'] = 'Duplicated in filedir and external storage';
 $string['object_status:location:local'] = 'Only in filedir';
 $string['object_status:location:external'] = 'Only in external storage';
