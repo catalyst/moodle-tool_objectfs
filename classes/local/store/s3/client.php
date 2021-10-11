34250b2501717d14f69f43043352f8440748fd20
Vincent Rioux <vrioux@ctech.ca>
2021-10-11 09:14:30 -0400

    Remove whitespace

--- classes/local/store/s3/client.php
+++ classes/local/store/s3/client.php
@@ -457,7 +457,7 @@ class client extends object_client_base {
         $key = $this->get_filepath_from_hash($contenthash);
         $params['Bucket'] = $this->bucket;
         $params['Key'] = $this->bucketkeyprefix . $key;
-        
+
         $contentdisposition = manager::get_header($headers, 'Content-Disposition');
         if ($contentdisposition !== '') {
             $params['ResponseContentDisposition'] = $contentdisposition;
