43d49e548c36df1e5d4070f15aea6b2597375c12
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2026-04-08 11:11:31 +1000

    issue #736: add missing cache lang string

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026030200;      // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2026022401;      // Same as version.
+$plugin->version   = 2026040800;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2026040800;      // Same as version.
 $plugin->requires  = 2024042200;      // Requires 4.4.
 $plugin->component = "tool_objectfs";
 $plugin->maturity  = MATURITY_STABLE;
